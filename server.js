91ac2a3369cd215b464db95e92c510466a1820e1
Calyx <calyxdrey11@gmail.com>
2025-06-28 16:09:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// server.js - UPDATED
+// server.js
 const express = require('express');
 const http = require('http');
 const { Server } = require("socket.io");
@@ -9,62 +9,65 @@ const io = new Server(server);
 
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the 'public' folder
 app.use(express.static('public'));
 
 let users = {};
-// --- NEW: Store the last 100 messages ---
-const messageHistory = [];
-const MAX_HISTORY_LENGTH = 100;
+let messageHistory = [];
+const HISTORY_LIMIT = 50;
+
+// Helper to add a message to history
+const addMessageToHistory = (messageObject) => {
+    messageHistory.push(messageObject);
+    if (messageHistory.length > HISTORY_LIMIT) {
+        messageHistory.shift(); // Remove the oldest message
+    }
+};
 
 io.on('connection', (socket) => {
     console.log('A user connected:', socket.id);
 
-    // When a user joins with a username
     socket.on('join', (username) => {
         users[socket.id] = username;
         socket.username = username;
 
-        // --- NEW: Send message history to the newly connected user ---
+        // Send existing chat history to the new user
         socket.emit('load history', messageHistory);
-
-        // Broadcast to all other users that a new person has joined
-        socket.broadcast.emit('system message', `${username} has joined the chat.`);
         
-        // Update the user list for everyone
+        const systemMessage = {
+            type: 'system',
+            msg: `${username} has joined the chat.`,
+            timestamp: new Date()
+        };
+        addMessageToHistory(systemMessage);
+        socket.broadcast.emit('system message', systemMessage);
+        
         io.emit('update users', Object.values(users));
     });
 
-    // When a chat message is received
     socket.on('chat message', (msg) => {
-        // Prevent chat if user hasn't joined yet
-        if (!socket.username) return;
-
-        const messageData = {
+        const messageObject = {
             username: socket.username,
-            msg: msg
+            msg: msg,
+            timestamp: new Date()
         };
-
-        // --- NEW: Add message to history ---
-        messageHistory.push(messageData);
-        // If history is too long, remove the oldest message
-        if (messageHistory.length > MAX_HISTORY_LENGTH) {
-            messageHistory.shift();
-        }
-
-        // Broadcast the message to everyone, including the sender
-        io.emit('chat message', messageData);
+        addMessageToHistory(messageObject);
+        io.emit('chat message', messageObject);
     });
 
-    // When a user disconnects
     socket.on('disconnect', () => {
         console.log('A user disconnected:', socket.id);
         if (users[socket.id]) {
             const username = users[socket.id];
             delete users[socket.id];
-            // Broadcast that the user has left
-            socket.broadcast.emit('system message', `${username} has left the chat.`);
-            // Update the user list for the remaining users
+            
+            const systemMessage = {
+                type: 'system',
+                msg: `${username} has left the chat.`,
+                timestamp: new Date()
+            };
+            addMessageToHistory(systemMessage);
+            socket.broadcast.emit('system message', systemMessage);
+
             io.emit('update users', Object.values(users));
         }
     });
